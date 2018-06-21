7781c9ee8fb2f5d890f196a49d4207c5c63a710e
Jagadish Pujari <jagadish.pujari@tarento.com>
2018-06-21 14:41:55 +0530

    Issue #2 feat: Added interfaces in question unit plugin.js

--- editor/plugin.js
+++ editor/plugin.js
@@ -6,9 +6,31 @@
  */
 org.ekstep.contenteditor.questionUnitPlugin = org.ekstep.contenteditor.basePlugin.extend({
   type: "org.ekstep.contenteditor.questionUnitPlugin",
+  _data: {},
   initialize: function () {
-  	CKEDITOR.basePath = ecEditor.resolvePluginResource(this.manifest.id, this.manifest.ver, "editor/libs/ck-editor/");
+    this.beforeInit();
+    if(this.manifest.id == 'org.ekstep.questionunit') {
+  	  CKEDITOR.basePath = ecEditor.resolvePluginResource(this.manifest.id, this.manifest.ver, "editor/libs/ck-editor/");
+    }
+    this.afterInit();
+  },
+  beforeInit: function() {
+
+  },
+  afterInit: function() {
+
+  },
+  renderForm: function(data) {
+    this._data = data;
+  },
+  validateForm: function(callback) {
+    var instance = this;
+    ecEditor.dispatchEvent(this.manifest.id + ":validateform", function(isValid, data) {
+      instance.__proto__.__proto__._data = data;
+      if(_.isFunction(callback)) {
+        callback(isValid);
+      }
+    });
   }
-  // TODO: Interfaces
 });
 //# sourceURL=questionUnitPlugin.js
