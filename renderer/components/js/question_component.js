0bf1fec690b651682956d08d1c88b31e4cb341f2
sivashanmugam-kannan <sivashanmugam.kannan@funtoot.com>
2018-08-20 18:18:12 +0530

    added question component which will show image audio and question text with expand feature

---
+++ renderer/components/js/question_component.js
@@ -0,0 +1,77 @@
+org.ekstep.contentrenderer.questionUnitPluginComponentObj = {
+    generateQuestionComponent: function () {
+        return '\
+        <div class="question-container">\
+        <% if(question.data.question.image || question.data.question.audio){ %> \
+            <div class="image-container">\
+            <% if(question.data.question.image){ %> \
+                <img onclick="QuestionUnitRendererController.pluginInstance.showImageModel(event, \'<%= question.data.question.image %>\')" class="q-image" src="<%= question.data.question.image %>" />\
+                <img onclick=QuestionUnitRendererController.pluginInstance.playAudio({src:"<%= question.data.question.audio %>"}) class="audio" src="<%= QuestionUnitRendererController.pluginInstance.getAudioIcon("renderer/assets/audio-icon.png") %>" />\
+            <% }else { %>\
+              <img onclick=QuestionUnitRendererController.pluginInstance.playAudio({src:"<%= question.data.question.audio %>"}) class="audio no-q-image" src="<%= QuestionUnitRendererController.pluginInstance.getAudioIcon("renderer/assets/audio-icon.png") %>" />\
+            <% } %>\
+            </div>\
+        <% } %>\
+            <div class="hiding-container">\
+                <div class="expand-container <% if(question.data.question.image || question.data.question.audio){ %> with-media <% } %>">\
+                <%= question.data.question.text %>\
+                </div>\
+            </div>\
+            <div class="expand-button" onclick="QuestionUnitRendererController.pluginInstance.toggleQuestionText()">\
+                <img src="<%= QuestionUnitRendererController.pluginInstance.getAudioIcon("renderer/assets/down_arrow.png") %>" />\
+            </div>\
+        </div>\
+        ';
+    },
+    isQuestionTextOverflow: function () {
+        setTimeout(function () {
+            if ($('.hiding-container').height() > $('.expand-container').height()) {
+                $('.expand-button').css('display', 'none');
+            } else {
+                $('.expand-button').css('display', 'block');
+            }
+        }, 1000)
+    },
+    toggleQuestionText: function () {
+        if ($('.hiding-container').hasClass('expanded')) {
+            $('.hiding-container').css('height', '50%');
+            $('.hiding-container').removeClass('expanded')
+            $(".expand-button img").toggleClass('flip');
+            $('.hiding-container').css('padding-bottom', '0px');
+            $('.expand-button').css('bottom', '5%');
+        } else {
+            var expandButtonBottom = parseFloat($('.expand-button').css('bottom'));
+            $('.hiding-container').addClass('expanded')
+            $('.hiding-container').css('height', 'auto');
+            $(".expand-button img").toggleClass('flip');
+            $('.hiding-container').css('padding-bottom', $(".expand-button").height() + 'px');
+            expandButtonBottom = expandButtonBottom - ($('.hiding-container').height() - $('.question-container').height());
+            $('.expand-button').css('bottom', expandButtonBottom + 'px')
+        }
+    },
+    onDomReady: function () {
+        this.isQuestionTextOverflow();
+    },
+    generateModelTemplate: function(){
+        return "<div class='popup' id='image-model-popup' onclick='QuestionUnitRendererController.pluginInstance.hideImageModel()'><div class='popup-overlay' onclick='QuestionUnitRendererController.pluginInstance.hideImageModel()'></div> \
+        <div class='popup-full-body'> \
+            <div class='font-lato assess-popup assess-goodjob-popup'> \
+                <img class='qc-question-fullimage' src=<%= src %> /> \
+                <div onclick='QuestionUnitRendererController.pluginInstance.hideImageModel()' class='qc-popup-close-button'>&times;</div> \
+            </div>\
+        </div>"
+    },
+    showImageModel: function (event, imageSrc) {
+        if (imageSrc) {
+            var modelTemplate = this.generateModelTemplate();
+            var template = _.template(modelTemplate);
+            var templateData = template({
+                src: imageSrc
+            })
+            $(QuestionUnitRendererController.constant.qContainerElement).append(templateData);
+        }
+    },
+    hideImageModel: function () {
+        $("#image-model-popup").remove();
+    }
+}
--- renderer/plugin.js
+++ renderer/plugin.js
@@ -19,6 +19,7 @@ org.ekstep.contentrenderer.questionUnitPlugin = Plugin.extend({
     EkstepRendererAPI.addEventListener(this._manifest.id + ":hide", this.hideQuestion, this);
     EkstepRendererAPI.addEventListener(this._manifest.id + ":evaluate", this.evaluateQuestion, this);
     EkstepRendererAPI.addEventListener(this._manifest.id + ":rendermath", this.renderMath, this);
+    this.loadComponents();
   },
   /**
    * Listener for ':show' event.
@@ -209,6 +210,9 @@ org.ekstep.contentrenderer.questionUnitPlugin = Plugin.extend({
         katex.render(mathText, jQuery(element)[0], { displayMode: true });
       });
     }, 600);
+  },
+  loadComponents: function(){
+    jQuery.extend(this, org.ekstep.contentrenderer.questionUnitPluginComponentObj);
   }
 });
 //# sourceURL=questionUnitRenderer.js
