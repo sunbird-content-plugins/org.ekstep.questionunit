473fcf02bc1f2552129781b54a795d7369cd7c6d
Ram Jayaraman <ram.j@funtoot.com>
2018-08-27 14:30:20 +0530

    combined all the component files into one file to take care of loading time

--- renderer/components/js/components.js
+++ renderer/components/js/components.js
@@ -1,4 +1,36 @@
 org.ekstep.questionunit = org.ekstep.questionunit || {};
+org.ekstep.questionunit.baseComponent = {
+    playAudio: function (audioObj) {
+        EkstepRendererAPI.dispatchEvent('org.ekstep.questionunit:playaudio', audioObj)
+    },
+    loadImageFromUrl: function (element, imgUrl, pluginId, pluginVer) {
+        EkstepRendererAPI.dispatchEvent('org.ekstep.questionunit:loadimagefromurl', { 'element': element, 'path': imgUrl, 'pluginId': pluginId, 'pluginVer': pluginVer });
+    },
+    generateModelTemplate: function () {
+        return "<div class='popup' id='image-model-popup' onclick='org.ekstep.questionunit.questionComponent.hideImageModel()'><div class='popup-overlay' onclick='org.ekstep.questionunit.questionComponent.hideImageModel()'></div> \
+        <div class='popup-full-body'> \
+            <div class='font-lato assess-popup assess-goodjob-popup'> \
+                <img class='qc-question-fullimage' src=<%= src %> /> \
+                <div onclick='org.ekstep.questionunit.questionComponent.hideImageModel()' class='qc-popup-close-button'>&times;</div> \
+            </div>\
+        </div>"
+    },
+    showImageModel: function (event, imageSrc) {
+        if (imageSrc) {
+            var modelTemplate = this.generateModelTemplate();
+            var template = _.template(modelTemplate);
+            var templateData = template({
+                src: imageSrc
+            })
+            $('.plugin-content-container').append(templateData);
+        }
+    },
+    hideImageModel: function () {
+        $("#image-model-popup").remove();
+    }
+}
+
+org.ekstep.questionunit = org.ekstep.questionunit || {};
 org.ekstep.questionunit.questionComponent = {
     generateQuestionComponent: function () {
         return '\
@@ -57,4 +89,20 @@ org.ekstep.questionunit.questionComponent = {
     }
 }
 jQuery.extend(org.ekstep.questionunit.questionComponent, org.ekstep.questionunit.baseComponent);
-//# sourceURL=org.ekstep.questionunit.questionComponent.js
+
+org.ekstep.questionunit = org.ekstep.questionunit || {};
+org.ekstep.questionunit.backgroundComponent = {
+    settings: {
+        bgColors: ["#5DC4F5", "#FF7474", "#F9A817", "#48DCB6", "#5B6066"],
+        bgColor: "#5DC4F5"
+    },
+    getBackgroundGraphics: function () {
+        org.ekstep.questionunit.backgroundComponent.settings.bgColor = org.ekstep.questionunit.backgroundComponent.settings.bgColors[_.random(0, org.ekstep.questionunit.backgroundComponent.settings.bgColors.length - 1)];
+        return '\
+            <div class="bg-graphics" style="background-color:<%= org.ekstep.questionunit.backgroundComponent.settings.bgColor %>">\
+                <div class="bg-circle circle-left" style="top:<%= _.random(-6, 6)*10%>vh" ></div ><div class="bg-circle circle-right" style="top:<%= _.random(-6, 6)*10%>vh"></div>\
+            </div >'
+    }
+};
+
+//# sourceURL=org.ekstep.questionunit.components.js
